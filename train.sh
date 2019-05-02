150c0553a9ebcdf8ed78636306dff91ac3d31e33
Kristofer Schlachter <kristofe72@gmail.com>
2019-05-02 17:33:37 -0400

    Better training script

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python pix2pix.py --mode train --output_dir output --max_epochs 200 --input_dir dataset/paper/tr --which_dir AtoB --png16bits
+CUDA_VISIBLE_DEVICES=0 python pix2pix.py --mode train --output_dir output --max_epochs 200 --input_dir dataset/paper/tr --which_dir AtoB --png16bits --display_freq 100
